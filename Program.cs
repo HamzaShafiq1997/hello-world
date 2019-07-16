751973979e92f0f9ba7566c9b03371f1a0a95c10
HamzaShafiq1997 <52954489+HamzaShafiq1997@users.noreply.github.com>
2019-07-16 05:06:26 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Transformer trans = new Transformer(); //Error
+
+            Transformer trans = new Car();
+            trans.run();
+            trans.show();
+
+            trans = new Boat();
+            trans.run();
+
+            trans = new Halicopter();
+            trans.run();
+        }
+    }
+}
---
+++ Transformer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class
+{
+    abstract class Transformer
+    {
+      
+
+        public Transformer()
+        {
+            
+            Console.WriteLine("Transformer Constructor.");
+        }
+
+        public void show()
+        {
+            Console.WriteLine("show transformer");
+        }
+        public abstract void run();
+        
+    }
+
+    class Car : Transformer
+    {
+        public override void run()
+        {
+            Console.WriteLine("Run Car");
+        }
+    }
+
+    class Boat : Transformer
+    {
+        public override void run()
+        {
+            Console.WriteLine("Run Boat");
+        }
+    }
+
+    class Halicopter : Transformer
+    {
+        public override void run()
+        {
+            Console.WriteLine("Run Halicopter");
+        }
+    }
+
+
+}
